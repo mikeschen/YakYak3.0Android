2fb6f45aa6f3e6757417e6853d3d74f12026c36f
Michael Schennum <mikeschenphoto@gmail.com>
2016-05-03 15:48:00 -0700

    adds replys to posts in firebase

--- app/src/main/java/com/mikeschen/www/yakyak/ui/PostActivity.java
+++ app/src/main/java/com/mikeschen/www/yakyak/ui/PostActivity.java
@@ -3,6 +3,7 @@ package com.mikeschen.www.yakyak.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +33,7 @@ public class PostActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
         ButterKnife.bind(this);
+        mReplyButton.setOnClickListener(this);
 
 
         int startingPosition = Integer.parseInt(getIntent().getStringExtra("position"));
@@ -43,6 +45,16 @@ public class PostActivity extends AppCompatActivity implements View.OnClickListe
         public void onClick(View view) {
             if(view == mReplyButton) {
                 String reply = mReplyEditText.getText().toString();
+                saveReplyToFirebase(reply);
+                Toast.makeText(PostActivity.this, "Replied", Toast.LENGTH_SHORT).show();
         }
+    }
+    public void saveReplyToFirebase(String reply) {
+        int startingPosition = Integer.parseInt(getIntent().getStringExtra("position"));
+        mPosts = Parcels.unwrap(getIntent().getParcelableExtra("posts"));
+        Firebase addedReplyRef = new Firebase(Constants.FIREBASE_URL_ADDED_POST);
+        String replyRef = mPosts.get(startingPosition).getPushId();
+        Log.d("replyref", replyRef);
+        addedReplyRef.child(replyRef).push().setValue(reply);
     }
 }
